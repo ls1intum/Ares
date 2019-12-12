dfd60407e024648262cb8b20ba42477047f3f9c6
Christian Femers <c.femers@tum.de>
2019-12-13 00:58:34 +0100

    Make AbstractLine public

--- src/main/java/de/tum/in/test/api/io/AbstractLine.java
+++ src/main/java/de/tum/in/test/api/io/AbstractLine.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 import de.tum.in.test.api.localization.Messages;
 
-abstract class AbstractLine implements Line {
+public abstract class AbstractLine implements Line {
 
 	int lineNumber = -1;
 
