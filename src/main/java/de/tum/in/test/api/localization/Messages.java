e59781f5e8f76f67e6d609dcf7d2da49bcdbc4ce
Christian Femers <c.femers@tum.de>
2020-03-26 20:51:59 +0100

    Make Messages final

--- src/main/java/de/tum/in/test/api/localization/Messages.java
+++ src/main/java/de/tum/in/test/api/localization/Messages.java
@@ -3,7 +3,7 @@ package de.tum.in.test.api.localization;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class Messages {
+public final class Messages {
 	private static final String BUNDLE_NAME = "de.tum.in.test.api.localization.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
