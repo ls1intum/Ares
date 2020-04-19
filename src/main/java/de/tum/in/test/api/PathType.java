a3d505a78f2b41ba059a7a34c2ff05f0883c8ea6
Christian Femers <c.femers@tum.de>
2020-04-19 06:29:33 +0200

    Make relative GLOB patterns work together with dots

--- src/main/java/de/tum/in/test/api/PathType.java
+++ src/main/java/de/tum/in/test/api/PathType.java
@@ -1,10 +1,19 @@
 package de.tum.in.test.api;
 
+import java.io.File;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.platform.commons.util.LruCache;
 
 public enum PathType {
 	/**
@@ -34,8 +43,9 @@ public enum PathType {
 	GLOB {
 		@Override
 		public PathMatcher convertToPathMatcher(String s) {
-			PathMatcher pm = DEFAULT_FS.getPathMatcher("glob:" + s);
-			return p -> pm.matches(relativizeSafe(p));
+			GlobNormalizationResult normalizedGlob = GlobNormalizationResult.normalizeGlobPattern(s);
+			PathMatcher pm = DEFAULT_FS.getPathMatcher("glob:" + normalizedGlob.getNormalizedGlobPattern());
+			return p -> pm.matches(relativizeSafe(p, normalizedGlob.getRelativeOffset()));
 		}
 	},
 	/**
@@ -47,7 +57,7 @@ public enum PathType {
 		@Override
 		public PathMatcher convertToPathMatcher(String s) {
 			PathMatcher pm = DEFAULT_FS.getPathMatcher("regex:" + s);
-			return p -> pm.matches(relativizeSafe(p));
+			return p -> pm.matches(relativizeSafe(p, NO_OFFSET));
 		}
 	},
 	/**
@@ -82,13 +92,80 @@ public enum PathType {
 	 */
 	public abstract PathMatcher convertToPathMatcher(String s);
 
-	static final Path CURRENT_PATH = Path.of("").toAbsolutePath();
 	static final FileSystem DEFAULT_FS = FileSystems.getDefault();
+	static final Path CURRENT_PATH = Path.of("").toAbsolutePath();
+	static final List<Path> CURRENT_PATH_HIERARCHY = Stream.iterate(CURRENT_PATH, Path::getParent)
+			.takeWhile(Objects::nonNull).collect(Collectors.toUnmodifiableList());
 
-	private static Path relativizeSafe(Path any) {
+	private static final int NO_OFFSET = 0;
+	private static final Pattern GLOB_SINGLE_DOT_ELIMINATION;
+	private static final Pattern GLOB_DOUBLE_DOT_ELIMINATION;
+
+	static {
+		String sepPat = "/".equals(File.separator) ? "/" : "[/\\\\]";
+		String nonSepPat = "/".equals(File.separator) ? "(?!\\.\\./)[^/]+" : "(?!\\.\\.[/\\\\])(?:[^/\\\\])+";
+		GLOB_SINGLE_DOT_ELIMINATION = Pattern
+				.compile(String.format("^(?:\\.(?:%1$s|$))+|(?:%1$s\\.)(?=%1$s|$)|%1$s$", sepPat));
+		GLOB_DOUBLE_DOT_ELIMINATION = Pattern
+				.compile(String.format("^%2$s%1$s\\.\\.(?:%1$s|$)|%1$s%2$s%1$s\\.\\.(?=%1$s|$)", sepPat, nonSepPat));
+	}
+
+	private static Path relativizeSafe(Path any, int offset) {
 		Path p = any.normalize().toAbsolutePath();
 		if (!Objects.equals(p.getRoot(), CURRENT_PATH.getRoot()))
 			return p;
-		return CURRENT_PATH.relativize(p).normalize();
+		return CURRENT_PATH_HIERARCHY.get(offset).relativize(p).normalize();
+	}
+
+	static final class GlobNormalizationResult {
+
+		private static final int CACHE_SIZE = 256;
+		private static final Map<String, GlobNormalizationResult> CACHE = new LruCache<>(CACHE_SIZE - 1);
+
+		private final int relativeOffset;
+		private final String normalizedGlobPattern;
+
+		private GlobNormalizationResult(String globPattern) {
+			String cleaned;
+			cleaned = GLOB_SINGLE_DOT_ELIMINATION.matcher(globPattern).replaceAll("");
+			Matcher m;
+			while ((m = GLOB_DOUBLE_DOT_ELIMINATION.matcher(cleaned)).find()) {
+				cleaned = m.replaceAll("");
+			}
+			int offset = 0;
+			while (cleaned.startsWith("..")) {
+				if (cleaned.length() == 2)
+					cleaned = "";
+				else
+					cleaned = cleaned.substring(3);
+				offset++;
+			}
+			if (offset >= CURRENT_PATH_HIERARCHY.size())
+				throw new IllegalArgumentException("relative glob pattern for current path requires offset " + offset);
+			this.relativeOffset = offset;
+			this.normalizedGlobPattern = cleaned;
+		}
+
+		int getRelativeOffset() {
+			return relativeOffset;
+		}
+
+		String getNormalizedGlobPattern() {
+			return normalizedGlobPattern;
+		}
+
+		static GlobNormalizationResult normalizeGlobPattern(String globPattern) {
+			GlobNormalizationResult result;
+			synchronized (CACHE) {
+				result = CACHE.get(globPattern);
+			}
+			if (result == null) {
+				result = new GlobNormalizationResult(globPattern);
+				synchronized (CACHE) {
+					CACHE.put(globPattern, result);
+				}
+			}
+			return result;
+		}
 	}
 }
