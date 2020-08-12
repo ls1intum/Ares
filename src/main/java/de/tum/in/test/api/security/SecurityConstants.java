d29f702300448d858fd9ebd9f568333143f6cc3b
Christian Femers <c.femers@tum.de>
2020-08-12 06:21:56 +0200

    Add code coverage analysis

--- src/main/java/de/tum/in/test/api/security/SecurityConstants.java
+++ src/main/java/de/tum/in/test/api/security/SecurityConstants.java
@@ -14,13 +14,14 @@ public final class SecurityConstants {
 
 	static final Set<String> STACK_WHITELIST = Set.of("java.", "org.junit.", "jdk.", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 			"org.eclipse.", "com.intellij", "org.assertj", "org.opentest4j.", // $NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-			"com.sun.", "sun.", "org.apache.", "de.tum.in.test.", "net.jqwik", "ch.qos.logback", SECURITY_PACKAGE_NAME); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+			"com.sun.", "sun.", "org.apache.", "de.tum.in.test.", "net.jqwik", "ch.qos.logback", "org.jacoco", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+			SECURITY_PACKAGE_NAME);
 	static final Set<String> STACK_BLACKLIST = Set.of(BlacklistedInvoker.class.getName());
 
 	static final Set<String> PACKAGE_USE_BLACKLIST = Set.of(SECURITY_PACKAGE_NAME, "java.lang.reflect",
 			"de.tum.in.test.api.internal", "jdk.internal");
-	
+
 	private SecurityConstants() {
-		
+
 	}
 }
