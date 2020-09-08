8ada4ef992df98a7cdcfc3fe2a88aa3058a2205c
Christian Femers <c.femers@tum.de>
2020-09-08 13:44:36 +0200

    Fix #43

--- src/main/java/de/tum/in/test/api/WhitelistPackage.java
+++ src/main/java/de/tum/in/test/api/WhitelistPackage.java
@@ -14,29 +14,26 @@ import org.apiguardian.api.API.Status;
 import de.tum.in.test.api.security.ArtemisSecurityManager;
 
 /**
- * Allows to whitelist a package, possibly including all subpackages. The
+ * Allows to whitelist a package, possibly including all subpackages. Only
+ * useful in combination with {@link BlacklistPackage}. The
  * {@link ArtemisSecurityManager} will allow access for <b>non-whitelisted
- * callers</b> outside the set of whitelisted packages. This annotation is
+ * callers</b> to the set of whitelisted packages. This annotation is
  * {@linkplain Repeatable}, and can be placed additively on the test class and
  * test method.
  * <p>
  * You can use <code>*</code> and <code>**</code> in the same way as in GLOB
  * patterns just applied to packages where the delimiter is <code>"."</code>.
  * <p>
- * Use e.g. <code>@BlacklistPackage("java.util")</code> to allow access to all
- * classes in the package <code>java.util</code> and
- * <code>@BlacklistPackage("java.util**")</code> to allow access to classes in
- * <code>java.util</code> and all its subpackages.
- * <p>
- * The access to <code>java.lang</code> is always allowed and by default, all
- * packages can be used, apart from java.lang.reflect, and internal packages of
- * AJTS.
+ * Use e.g. <code>@BlacklistPackage("java.util**")</code> and then
+ * <code>@WhitelistPackage("java.util.function")</code> to allow access to all
+ * classes in the package <code>java.util.function</code> but not any other
+ * classes in <code>java.util</code> or any of its subclasses.
  * <p>
  * <b>This annotation overpowers any {@link BlacklistPackage} annotations.</b>
  *
  * @author Christian Femers
  * @since 0.5.1
- * @version 1.1.0
+ * @version 1.1.1
  */
 @API(status = Status.EXPERIMENTAL)
 @Documented
