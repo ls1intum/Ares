ac126a507aa4a057bddd00a0119c7d423ef598f7
Christian Femers <c.femers@tum.de>
2020-10-21 09:39:34 +0200

    Add tests for new security configuration builder validation

---
+++ src/test/java/de/tum/in/testuser/ValidationUser.java
@@ -0,0 +1,87 @@
+package de.tum.in.testuser;
+
+import org.junit.jupiter.api.Test;
+
+import de.tum.in.test.api.AllowLocalPort;
+import de.tum.in.test.api.AllowThreads;
+import de.tum.in.test.api.jupiter.Public;
+
+public class ValidationUser {
+
+	@AllowLocalPort(value = { 21, 80 }, allowPortsAbove = 21, exclude = { 21, 22 })
+	@Public
+	@Test
+	void allowAndExcludeLocalPortIntersect() {
+		// nothing
+	}
+
+	@AllowLocalPort(value = { 21, 80 }, allowPortsAbove = 10)
+	@Public
+	@Test
+	void allowLocalPortInsideAllowAboveRange() {
+		// nothing
+	}
+
+	@AllowLocalPort(allowPortsAbove = 80, exclude = { 8080, 22 })
+	@Public
+	@Test
+	void excludeLocalPortValueToSmall() {
+		// nothing
+	}
+
+	@AllowLocalPort(exclude = 22)
+	@Public
+	@Test
+	void excludeLocalPortValueWithoutAllowAbove() {
+		// nothing
+	}
+
+	@AllowLocalPort(allowPortsAbove = -1)
+	@Public
+	@Test
+	void negativeAllowLocalPortAboveValue() {
+		// nothing
+	}
+
+	@AllowLocalPort(value = -1)
+	@Public
+	@Test
+	void negativeAllowLocalPortValue() {
+		// nothing
+	}
+
+	@AllowLocalPort(allowPortsAbove = 10, exclude = -1)
+	@Public
+	@Test
+	void negativeExcludeLocalPortValue() {
+		// nothing
+	}
+
+	@AllowThreads(maxActiveCount = -1)
+	@Public
+	@Test
+	void negativeMaxActiveCount() {
+		// nothing
+	}
+
+	@AllowLocalPort(allowPortsAbove = 100_000)
+	@Public
+	@Test
+	void tooLargeAllowLocalPortAboveValue() {
+		// nothing
+	}
+
+	@AllowLocalPort(value = 100_000)
+	@Public
+	@Test
+	void tooLargeAllowLocalPortValue() {
+		// nothing
+	}
+
+	@AllowLocalPort(allowPortsAbove = 10, exclude = 100_000)
+	@Public
+	@Test
+	void tooLargeExcludeLocalPortValue() {
+		// nothing
+	}
+}
