2b892524a8c6de9d4b4ea526571ee097d8bd7630
Christian Femers <c.femers@tum.de>
2020-10-22 06:48:05 +0200

    Improve and fix setting localization for test classes

--- src/main/java/de/tum/in/test/api/localization/LocaleExtension.java
+++ src/main/java/de/tum/in/test/api/localization/LocaleExtension.java
@@ -7,26 +7,34 @@ import org.apiguardian.api.API.Status;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import org.junit.platform.commons.support.AnnotationSupport;
 
 @API(status = Status.INTERNAL)
 public class LocaleExtension implements BeforeAllCallback, AfterAllCallback {
 
-	private Locale oldLocale;
+	private static final String OLD_LOCALE_KEY = "old-locale";
 
 	@Override
 	public void beforeAll(ExtensionContext context) throws Exception {
 		var annot = AnnotationSupport.findAnnotation(context.getElement(), UseLocale.class);
 		if (annot.isEmpty())
 			return;
-		oldLocale = Locale.getDefault();
+		getStore(context).put(OLD_LOCALE_KEY, Locale.getDefault());
 		var newLocale = new Locale(annot.get().value());
 		Locale.setDefault(newLocale);
 	}
 
 	@Override
 	public void afterAll(ExtensionContext context) throws Exception {
-		if (oldLocale != null)
+		var oldLocale = getStore(context).remove(OLD_LOCALE_KEY, Locale.class);
+		if (oldLocale != null) {
 			Locale.setDefault(oldLocale);
+		}
+	}
+
+	private Store getStore(ExtensionContext context) {
+		return context.getStore(Namespace.create(getClass(), context.getElement()));
 	}
 }
--- src/main/java/de/tum/in/test/api/localization/Messages.java
+++ src/main/java/de/tum/in/test/api/localization/Messages.java
@@ -1,23 +1,42 @@
 package de.tum.in.test.api.localization;
 
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Locale.Category;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
+import org.junit.platform.commons.util.LruCache;
 
 @API(status = Status.INTERNAL)
 public final class Messages {
+
 	private static final String BUNDLE_NAME = "de.tum.in.test.api.localization.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	/**
+	 * Must match the localizations in the resources.
+	 */
+	private static final Set<Locale> SUPPORTED_LOCALES = Set.of(Locale.ROOT, Locale.GERMAN);
+
+	private static Map<Locale, ResourceBundle> resourceBundleCache = Collections.synchronizedMap(new LruCache<>(100));
+
+	static {
+		for (Locale locale : SUPPORTED_LOCALES) {
+			resourceBundleCache.put(locale, loadBundleForLocale(locale));
+		}
+	}
 
 	private Messages() {
+
 	}
 
 	public static String localized(String key) {
 		try {
-			return RESOURCE_BUNDLE.getString(key);
+			return getBundleForCurrentLocale().getString(key);
 		} catch (@SuppressWarnings("unused") MissingResourceException e) {
 			return '!' + key + '!';
 		}
@@ -25,10 +44,18 @@ public final class Messages {
 
 	public static String formatLocalized(String key, Object... args) {
 		try {
-			return String.format(RESOURCE_BUNDLE.getString(key), args);
+			return String.format(getBundleForCurrentLocale().getString(key), args);
 		} catch (@SuppressWarnings("unused") MissingResourceException e) {
 			return '!' + key + '!';
 		}
+	}
+
+	private static ResourceBundle getBundleForCurrentLocale() {
+		return resourceBundleCache.computeIfAbsent(Locale.getDefault(Category.DISPLAY), Messages::loadBundleForLocale);
+	}
+
+	private static ResourceBundle loadBundleForLocale(Locale locale) {
+		return ResourceBundle.getBundle(BUNDLE_NAME, locale);
 	}
 
 	public static void init() {
