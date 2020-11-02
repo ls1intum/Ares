59fcb4f4490dbb73b8343762d4376b0120c34504
Christian Femers <c.femers@tum.de>
2020-11-02 23:13:54 +0100

    Add annotations for defining trusted packages/classes

---
+++ src/main/java/de/tum/in/test/api/AddTrustedPackage.java
@@ -0,0 +1,38 @@
+package de.tum.in.test.api;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+/**
+ * Marks a package structure as trusted for the test scope the annotation is
+ * applied to. The classes in the packages have therefore more freedom and are
+ * less restricted by the SecurityManager.This annotation is
+ * {@linkplain Repeatable}, and can be placed additively on the test class and
+ * test method.
+ * <p>
+ * You can use <code>*</code> and <code>**</code> in the same way as in GLOB
+ * patterns just applied to packages where the delimiter is <code>"."</code>.
+ * <p>
+ * Use e.g. <code>@AddTrustedPackages("org.powermock**")</code> to whitelist all
+ * classes in any <code>org.powermock</code> package.
+ *
+ * @author Christian Femers
+ * @since 1.3.1
+ * @version 1.0.0
+ */
+@API(status = Status.MAINTAINED)
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, ANNOTATION_TYPE })
+@Repeatable(AddTrustedPackages.class)
+public @interface AddTrustedPackage {
+	String[] value();
+}
---
+++ src/main/java/de/tum/in/test/api/AddTrustedPackages.java
@@ -0,0 +1,28 @@
+package de.tum.in.test.api;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+/**
+ * Container annotation for {@linkplain Repeatable} {@link AddTrustedPackage}.
+ *
+ * @see AddTrustedPackage
+ * @author Christian Femers
+ * @since 1.3.1
+ * @version 1.0.0
+ */
+@API(status = Status.MAINTAINED)
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, ANNOTATION_TYPE })
+public @interface AddTrustedPackages {
+	AddTrustedPackage[] value();
+}
