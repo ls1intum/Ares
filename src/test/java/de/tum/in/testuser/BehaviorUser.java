83f28c70b8dcbc0d6febf7b5d77d91b7a874612f
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-11-17 00:59:04 +0100

    Suppress public modifier warning

--- src/test/java/de/tum/in/testuser/BehaviorUser.java
+++ src/test/java/de/tum/in/testuser/BehaviorUser.java
@@ -11,7 +11,7 @@ import de.tum.in.testuser.subject.structural.SomeClass;
 @Public
 @StrictTimeout(10)
 @WhitelistPath("")
-@SuppressWarnings("squid:S2699") // No assertions in tests
+@SuppressWarnings({"squid:S2699", "squid:S5786"}) // No assertions in tests, public modifier
 public class BehaviorUser extends BehaviorTest {
 
 	private static final String SUBJECT_PACKAGE = "de.tum.in.testuser.subject.structural";
