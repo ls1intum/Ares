8559d4d7d2678b1edaf34e7b3439e5117c6938b0
Christian Femers <c.femers@tum.de>
2021-03-19 17:15:29 +0100

    Order the test methods by name like they are ordered in the test class

--- src/test/java/de/tum/in/test/testutilities/UserBased.java
+++ src/test/java/de/tum/in/test/testutilities/UserBased.java
@@ -9,6 +9,8 @@ import java.lang.annotation.Target;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
+import org.junit.jupiter.api.MethodOrderer.MethodName;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.engine.descriptor.JupiterEngineDescriptor;
 
@@ -17,11 +19,12 @@ import org.junit.jupiter.engine.descriptor.JupiterEngineDescriptor;
  * user of the test API, so the user is a different test class that is not
  * called "Test" because it is not the main test itself, but executed using the
  * JUnit platform.
- * 
+ *
  * @author Christian Femers
  */
 @API(status = Status.INTERNAL)
 @ExtendWith(TestUserExtension.class)
+@TestMethodOrder(MethodName.class)
 @Documented
 @Retention(RUNTIME)
 @Target({ TYPE, ANNOTATION_TYPE })
