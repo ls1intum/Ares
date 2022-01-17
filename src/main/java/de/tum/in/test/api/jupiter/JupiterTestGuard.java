d081f82fdb1b57ca4e0669201e1346661067de79
Christian Femers <c.femers@tum.de>
2022-01-17 14:47:45 +0100

    Remove useless experimental method display name obfuscation.

--- src/main/java/de/tum/in/test/api/jupiter/JupiterTestGuard.java
+++ src/main/java/de/tum/in/test/api/jupiter/JupiterTestGuard.java
@@ -1,22 +1,16 @@
 package de.tum.in.test.api.jupiter;
 
 import static de.tum.in.test.api.internal.ReportingUtils.doProceedAndPostProcess;
-import static de.tum.in.test.api.internal.TestGuardUtils.*;
-import static de.tum.in.test.api.localization.Messages.formatLocalized;
-import static org.junit.platform.commons.support.AnnotationSupport.findAnnotation;
+import static de.tum.in.test.api.internal.TestGuardUtils.checkForHidden;
 
-import java.lang.reflect.Method;
-import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 
 import de.tum.in.test.api.Deadline;
-import de.tum.in.test.api.internal.TestType;
 
 /**
  * This class' main purpose is to guard the {@link HiddenTest}s execution and
@@ -25,9 +19,7 @@ import de.tum.in.test.api.internal.TestType;
  * @author Christian Femers
  */
 @API(status = Status.INTERNAL)
-public final class JupiterTestGuard implements UnifiedInvocationInterceptor, DisplayNameGenerator {
-
-	private final DisplayNameGenerator defaultNameGen = new DisplayNameGenerator.ReplaceUnderscores();
+public final class JupiterTestGuard implements UnifiedInvocationInterceptor {
 
 	@Override
 	public <T> T interceptGenericInvocation(Invocation<T> invocation, ExtensionContext extensionContext,
@@ -35,29 +27,5 @@ public final class JupiterTestGuard implements UnifiedInvocationInterceptor, Dis
 		JupiterContext jupiterContext = JupiterContext.of(extensionContext);
 		checkForHidden(jupiterContext);
 		return doProceedAndPostProcess(invocation, jupiterContext);
-	}
-
-	@Override
-	public String generateDisplayNameForClass(Class<?> testClass) {
-		return defaultNameGen.generateDisplayNameForClass(testClass);
-	}
-
-	@Override
-	public String generateDisplayNameForNestedClass(Class<?> nestedClass) {
-		return defaultNameGen.generateDisplayNameForNestedClass(nestedClass);
-	}
-
-	@Override
-	public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
-		if (findAnnotation(testMethod, JupiterArtemisTest.class).map(JupiterArtemisTest::value)
-				.orElse(null) == TestType.HIDDEN) {
-			var deadline = extractDeadline(Optional.of(testClass), Optional.of(testMethod));
-			String name = testMethod.toString();
-			if (deadline.isEmpty())
-				return formatLocalized("test_guard.obfuscate_hidden_test_missing_deadline", name); //$NON-NLS-1$
-			if (ZonedDateTime.now().isBefore(deadline.get()))
-				return String.format("Hidden Test %08X", name.hashCode()); //$NON-NLS-1$
-		}
-		return defaultNameGen.generateDisplayNameForMethod(testClass, testMethod);
 	}
 }
