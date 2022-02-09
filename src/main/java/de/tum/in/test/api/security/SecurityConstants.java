2349cf950afd08e9072623c28f26dadc7a7d6ff1
Christian Femers <c.femers@tum.de>
2022-02-09 18:28:05 +0100

    Remove redundant entry from stack whitelist

--- src/main/java/de/tum/in/test/api/security/SecurityConstants.java
+++ src/main/java/de/tum/in/test/api/security/SecurityConstants.java
@@ -17,8 +17,8 @@ public final class SecurityConstants {
 	static final String SECURITY_PACKAGE_NAME = SecurityConstants.class.getPackageName();
 
 	static final Set<String> STACK_WHITELIST = Set.of("java.", "org.junit.", "jdk.", "org.eclipse.", "com.intellij", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
-			"org.assertj", "org.opentest4j.", "com.sun.", "sun.", "org.apache.", "de.tum.in.test.", "net.jqwik", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
-			"ch.qos.logback", "org.jacoco", "javax.", "org.json", SECURITY_PACKAGE_NAME); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"org.assertj", "org.opentest4j.", "com.sun.", "sun.", "org.apache.", "de.tum.in.test.api", "net.jqwik", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+			"ch.qos.logback", "org.jacoco", "javax.", "org.json"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	static final Set<String> STACK_BLACKLIST = Set.of(BlacklistedInvoker.class.getName(),
 			"org.junit.platform.commons.util.ReflectionUtils.getUnderlyingCause"); //$NON-NLS-1$
 
