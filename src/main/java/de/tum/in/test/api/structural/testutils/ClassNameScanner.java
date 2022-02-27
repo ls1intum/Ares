0b7cdfd8e12c5b823352cdbb620b87a3efc9c70c
Ole Vester <ole.vester@t-online.de>
2022-02-27 18:11:27 +0100

    Remove unused import

--- src/main/java/de/tum/in/test/api/structural/testutils/ClassNameScanner.java
+++ src/main/java/de/tum/in/test/api/structural/testutils/ClassNameScanner.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
