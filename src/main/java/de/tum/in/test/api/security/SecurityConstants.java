65b9365d9f066a4f546b59abfbd1490ed10a2e6f
Ole Vester <ole.vester@t-online.de>
2022-03-05 23:45:40 +0100

    Added Whitelisted Packages

--- src/main/java/de/tum/in/test/api/security/SecurityConstants.java
+++ src/main/java/de/tum/in/test/api/security/SecurityConstants.java
@@ -18,12 +18,14 @@ public final class SecurityConstants {
 
 	static final Set<String> STACK_WHITELIST = Set.of("java.", "org.junit.", "jdk.", "org.eclipse.", "com.intellij", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 			"org.assertj", "org.opentest4j.", "com.sun.", "sun.", "org.apache.", "de.tum.in.test.api", "net.jqwik", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
-			"ch.qos.logback", "org.jacoco", "javax.", "org.json", "org.gradle"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"ch.qos.logback", "org.jacoco", "javax.", "org.json", "org.gradle", "com.teamscale", "okhttp3", "retrofit2", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"shadow.org", "com.squareup", "okio.Okio", "shadow.spark");
 	static final Set<String> STACK_BLACKLIST = Set.of(BlacklistedInvoker.class.getName(),
 			"org.junit.platform.commons.util.ReflectionUtils.getUnderlyingCause"); //$NON-NLS-1$
 
 	static final Set<String> PACKAGE_USE_BLACKLIST = Set.of(SECURITY_PACKAGE_NAME, "de.tum.in.test.api.internal", //$NON-NLS-1$
-			"jdk.internal", "sun.", "org.gradle"); //$NON-NLS-1$ //$NON-NLS-2$
+			"jdk.internal", "sun.", "org.gradle", "com.teamscale", "retrofit2", "shadow.org", "com.squareup", //$NON-NLS-1$ //$NON-NLS-2$
+			"okio.Okio", "shadow.spark");
 
 	private SecurityConstants() {
 	}
